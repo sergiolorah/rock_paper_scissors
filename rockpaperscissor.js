9cf1906ae7c06504ff7e96466aca11288a9c40ef
Sergio Lora <sergiolorah99@gmail.com>
2025-12-27 23:53:19 -0400

    Added the playGame function to play 5 consecutive rounds and show the scores and winners

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -2,9 +2,7 @@
 use math.random then round the number to 2 digits
 then use conditionals between 0-0.33 0.34-0.66 0.67-1*/
 
-/*keep tracking of the scores with 2 variables*/
-const computerScore = 0;
-const humanScore = 0;
+
 
 function getComputerChoice(){
   let choice = Math.random();
@@ -25,48 +23,58 @@ function getHumanChoice(){
   return choice.toLowerCase();
 }
 
-/* Write a function with humanchoice and computerchoice as arguments and declare a winner, adding the score and show a message*/
-function playRound(computerchoice , humanchoice){
-  console.log(computerchoice);
-  console.log(humanchoice);
-  
-  
-  if (computerchoice === 'rock'){
-    switch (humanchoice) {
-      case 'paper':
-        humanScore++;
-        return 'You Win! paper beats rock.';
-      case 'scissors':
-        computerScore++;
-        return 'You Lose! rock beats scissors';
-      case 'rock':
-        return "It's a tie!"
-    }
-  } else if (computerchoice === 'paper'){
+/* Write a function that plays 5 rounds and keep track of the scores*/
+function playGame(){
+  /*keep tracking of the scores with 2 variables*/
+  let computerScore = 0;
+  let humanScore = 0;
+  /* Write a function with humanchoice and computerchoice as arguments and declare a winner, adding the score and show a message*/
+  function playRound(computerchoice , humanchoice){
+    console.log('Computer choice: ' + computerchoice);
+    console.log('Your choice: ' + humanchoice);
+    
+    if (computerchoice === 'rock'){
       switch (humanchoice) {
-        case 'rock':
-          computerScore++;
-          return 'You Lose! paper beats rock.';
-        case 'scissors':
-          humanScore++;
-          return 'You Win! scissors beats paper';
         case 'paper':
-          return "It's a tie!"
-    }
-  } else{
-      switch (humanchoice) {
-        case 'rock':
           humanScore++;
-          return 'You Win! rock beats scissors.';
-        case 'paper':
-          computerScore++;
-          return 'You Lose! scissors beats paper';
+          return 'You Win! paper beats rock.';
         case 'scissors':
+          computerScore++;
+          return 'You Lose! rock beats scissors';
+        case 'rock':
           return "It's a tie!"
+      }
+    } else if (computerchoice === 'paper'){
+        switch (humanchoice) {
+          case 'rock':
+            computerScore++;
+            return 'You Lose! paper beats rock.';
+          case 'scissors':
+            humanScore++;
+            return 'You Win! scissors beats paper';
+          case 'paper':
+            return "It's a tie!"
+      }
+    } else{
+        switch (humanchoice) {
+          case 'rock':
+            humanScore++;
+            return 'You Win! rock beats scissors.';
+          case 'paper':
+            computerScore++;
+            return 'You Lose! scissors beats paper';
+          case 'scissors':
+            return "It's a tie!"
+      }
     }
   }
+
+  for (let i = 1; i < 6; i++) {
+    console.log(playRound(getComputerChoice(),getHumanChoice()));
+    console.log(`Round ${i} Your score: ${humanScore} Computer score: ${computerScore}`);
+  }
+
 }
 
-console.log(playRound(getComputerChoice(),getHumanChoice()));
-console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
+playGame();
 
