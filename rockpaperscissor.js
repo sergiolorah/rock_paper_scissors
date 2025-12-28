7830925165e99b908c3af76aef322e600b3d7f18
Sergio Lora <sergiolorah99@gmail.com>
2025-12-27 23:40:02 -0400

    Added the logic to play 1 round and the scores

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -2,6 +2,10 @@
 use math.random then round the number to 2 digits
 then use conditionals between 0-0.33 0.34-0.66 0.67-1*/
 
+/*keep tracking of the scores with 2 variables*/
+const computerScore = 0;
+const humanScore = 0;
+
 function getComputerChoice(){
   let choice = Math.random();
   round = parseFloat(choice.toFixed(2));
@@ -14,13 +18,55 @@ function getComputerChoice(){
   }
 }
 
-console.log(getComputerChoice());
-
 /* Write a function that asks the user for a choice and stores it in a variable*/
 
 function getHumanChoice(){
   let choice = prompt("Choose rock, paper or scissors:");
-  return choice;
+  return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+/* Write a function with humanchoice and computerchoice as arguments and declare a winner, adding the score and show a message*/
+function playRound(computerchoice , humanchoice){
+  console.log(computerchoice);
+  console.log(humanchoice);
+  
+  
+  if (computerchoice === 'rock'){
+    switch (humanchoice) {
+      case 'paper':
+        humanScore++;
+        return 'You Win! paper beats rock.';
+      case 'scissors':
+        computerScore++;
+        return 'You Lose! rock beats scissors';
+      case 'rock':
+        return "It's a tie!"
+    }
+  } else if (computerchoice === 'paper'){
+      switch (humanchoice) {
+        case 'rock':
+          computerScore++;
+          return 'You Lose! paper beats rock.';
+        case 'scissors':
+          humanScore++;
+          return 'You Win! scissors beats paper';
+        case 'paper':
+          return "It's a tie!"
+    }
+  } else{
+      switch (humanchoice) {
+        case 'rock':
+          humanScore++;
+          return 'You Win! rock beats scissors.';
+        case 'paper':
+          computerScore++;
+          return 'You Lose! scissors beats paper';
+        case 'scissors':
+          return "It's a tie!"
+    }
+  }
+}
+
+console.log(playRound(getComputerChoice(),getHumanChoice()));
+console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
+
